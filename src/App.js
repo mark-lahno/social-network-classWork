aa339948ca0c7bc033d1f954a7a2b67cab846d97
Mark Lahno <marklahno.99@gamil.com>
2023-05-25 11:50:15 +0300

    first commit updated

--- src/App.js
+++ src/App.js
@@ -7,13 +7,39 @@ function App() {
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2d7bF1q2WBQnHUhqaR2OuxjFiv_EIWBwqow&usqp=CAU" alt="" />
     </header>
     <nav className='nav'>
-      <div>Profile</div>
-      <div>Messages</div>
-      <div>News</div>
-      <div>Music</div>
-      <div>Settings</div>
+
+
+  
+
+
+
+      <div>
+        <a href="">Profile</a>
+      </div>
+      <div>
+      <a href="">Messages</a>
+        </div>
+      <div>
+      <a href="">News</a>
+        </div>
+      <div>
+      <a href="">Music</a>
+        </div>
+      <div>
+      <a href="">Settings</a>
+        </div>
     </nav>
-    <div className='main'>Main content</div>
+    <div className='main'>
+      <div>ava + description</div>
+      <div>
+        MY posts
+        <div>New posts</div>
+      </div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
    </div>
   );
 }
